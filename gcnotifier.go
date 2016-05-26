84c9278eba8094e208923b5586115d4196e3fe98
Carlo Alberto Ferraris <cafxx@strayorange.com>
2016-05-27 06:37:02 +0900

    Remove AfterGCUntilCollected and fix docs

--- gcnotifier.go
+++ gcnotifier.go
@@ -1,8 +1,12 @@
 // Package gcnotifier provides a way to receive notifications after every time
 // garbage collection (GC) runs. This can be useful to instruct your code to
-// free additional memory resources that you may be using. To minimize the load
-// on the GC the code that runs after receiving the notification should try to
-// avoid allocations as much as possible.
+// free additional memory resources that you may be using.
+//
+// To minimize the load on the GC the code that runs after receiving the
+// notification should try to avoid allocations as much as possible, or at the
+// very least that the amount of new memory allocated is significantly smaller
+// than the amount of memory that has been "freed" by your code.
+
 package gcnotifier
 
 import "runtime"
@@ -15,7 +19,11 @@ type sentinel struct {
 // run. If a notification is not consumed before another GC runs only one of the
 // two notifications is sent. To stop the notifications you can safely close the
 // channel.
-func AfterGC() <-chan struct{} {
+//
+// A common use case for this is when you have a custom pool of objects: instead
+// of setting a maximum size you can leave it unbounded and then drop all or
+// some of them after every GC run (e.g. sync.Pool drops all objects during GC).
+func AfterGC() chan struct{} {
 	s := &sentinel{gcCh: make(chan struct{})}
 	runtime.SetFinalizer(s, finalizer)
 	return s.gcCh
@@ -30,15 +38,4 @@ func finalizer(obj interface{}) {
 	}
 	// we get here only if the channel was not closed
 	runtime.SetFinalizer(s, finalizer)
-}
-
-// AfterGCUntilReachable is like AfterGC, but the channel will be closed when
-// the object supplied as argument is garbage collected. No finalizer should be
-// set on the object before or after calling this method. Pay attention to not
-// inadvertently keep the object alive (e.g. by referencing it in a callback or
-// goroutine) or the object may never be collected.
-func AfterGCUntilCollected(obj interface{}) <-chan struct {
-  gcCh := AfterGC()
-	runtime.SetFinalizer(obj, func() { close(goCh) })
-	return goCh
 }
