dd290f5901fc421e9cc841ed60f4b216ec50c53e
Carlo Alberto Ferraris <cafxx@strayorange.com>
2016-06-04 08:41:52 +0900

    Add the buffering writer example

--- gcnotifier_test.go
+++ gcnotifier_test.go
@@ -1,9 +1,10 @@
 package gcnotifier
 
 import (
+	"io/ioutil"
 	"runtime"
-	"time"
 	"testing"
+	"time"
 )
 
 func TestAfterGC(t *testing.T) {
@@ -36,4 +37,53 @@ func TestAfterGC(t *testing.T) {
 			return
 		}
 	}
+}
+
+// ExampleAfterGC implements a time-based buffering io.Writer: data sent over
+// dataCh is buffered for up to 100ms, then flushed out in a single call to
+// out.Write and the buffer is reused. If GC runs, the buffer is flushed and
+// then discarded so that it can be collected during the next GC run. The
+// example is necessarily simplistic, a real implementation would be more
+// refined (e.g. on GC flush or resize the buffer based on a threshold,
+// perform asynchronous flushes, properly signal completions and propagate
+// errors, adaptively preallocate the buffer based on the previous capacity,
+// etc.)
+func ExampleAfterGC(t *testing.T) {
+	dataCh := make(chan []byte)
+	flushCh := time.Tick(100 * time.Millisecond)
+	doneCh := make(chan struct{})
+
+	out := ioutil.Discard
+
+	go func() {
+		var buf []byte
+
+		gcn := New()
+		defer gcn.Close()
+
+		for {
+			select {
+			case data := <-dataCh:
+				// received data to write to the buffer
+				buf = append(buf, data...)
+			case <-flushCh:
+				// time to flush the buffer (but reuse it for the next writes)
+				out.Write(buf)
+				buf = buf[:0]
+			case <-gcn.AfterGC():
+				// GC just ran: flush and then drop the buffer
+				out.Write(buf)
+				buf = nil
+			case <-doneCh:
+				// close the writer: flush the buffer and return
+				out.Write(buf)
+				return
+			}
+		}
+	}()
+
+	for i := 0; i < 1<<16; i++ {
+		dataCh <- make([]byte, 1024)
+	}
+	doneCh <- struct{}{}
 }
